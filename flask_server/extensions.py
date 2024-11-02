df5ba005b1eb2b59f603f1d4161eeaa109b7d7c3
sem <stchoi1018@gmail.com>
2024-11-02 16:58:23 -0700

    adding frontend

--- flask_server/extensions.py
+++ flask_server/extensions.py
@@ -3,6 +3,7 @@ from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_cors import CORS
 
 # Initialize extensions
 db = SQLAlchemy()
@@ -10,3 +11,5 @@ bcrypt = Bcrypt()
 migrate = Migrate()  # Flask-Migrate for handling database migrations
 login_manager = LoginManager()  # Flask-Login for handling user sessions
 mail = Mail()  # Flask-Mail for sending emails
+cors = CORS()  # Flask-CORS for handling Cross-Origin Resource Sharing
+
